450526903ef58c086c82aff675198fc094a18585
Riley Waugh <RWaugh9221@student.pps.net>
2017-03-02 09:31:15 -0800

    finished commenting 66

--- Problem65.py
+++ Problem65.py
@@ -1,56 +1,41 @@
+#Problem: just see it -- it's too hard to give context: https://projecteuler.net/problem=65
+#           What is the sum of the numerator of the 100th continued fraction convergent of e?
+
 import math
 from time import time
 
-def gcf(x,y):
-    #Euclid's method for finding greatest common factor
-    a = max(x,y)
-    b = x+y-a
-    r = 2
-    while r:
-        r=a%b
-        a=b
-        b=r
-    return a
-
-def flipFraction(frac):
-    #flips a fraction: f(x) = 1/x
-    return [frac[1], frac[0]]
-
-def addIntegerToFraction(frac, xnt):
-    #Adds a whole number to a fraction
-    return [frac[0]+xnt*frac[1],frac[1]]
-
-def reduceFraction(fpair):
-    #reduces a fraction
-    fac = gcf(fpair[0],fpair[1])
-    return [int(fpair[0]/fac), int(fpair[1]/fac)]
-
-def getNextFraction(frac, adder):
-    nfrac = reduceFraction(addIntegerToFraction(flipFraction(frac),adder))
-    return nfrac
-
-def sumNumeratorDigits(frac):
-    numer = frac[0]
+def sumDigits(x):
+    #Sums the digits of a large number
+    sx = str(x)
     ssum = 0
-    while numer >= 1:
-        ssum += numer%10
-        numer = int(numer/10)
+    for i in sx:
+        ssum += int(i)
     return ssum
 
 def main():
-
+    #The strategy: One odd thing to notice about continued fractions (yes I had to look this up for the next problem) is that
+    #the numerator of a convergent follows a pattern: if the numerator of the kth convergent is p(k) and a(m) is the mth coefficient of 
+    #the continued fraction, p(0) = a(0), p(1) = a(0)*a(1)+1, and p(n) = a(n)*p(n-1)+p(n-2). Using this recursive identity, we simply generate
+    #the first 100 coefficients for the continued fraction of e and iteratively go up and find p(100). Then, we sum the digitsm, and that's the answer
+    #executes in "0.0 seconds" (thanks garbage time library)
+    t0 = time()
     e_list = []
     maxVal = 100
     for i in range(2,1+maxVal):
         if i%3:
             e_list.append(1)
         else:
-            e_list.append(2*i/3)
-    startFrac = [e_list[-1],1]
-    for i in range(2,len(e_list)+1):
-        startFrac = getNextFraction(startFrac, e_list[-i])
-    startFrac = getNextFraction(startFrac, 2)
-    numeratorSum = int(sumNumeratorDigits(startFrac))
+            e_list.append(int(2*i/3))
+    
+    num = 3
+    pnum = 2
+    for i in range(1,len(e_list)):
+        nnum = e_list[i]*num+pnum
+        pnum = num
+        num = nnum
+
+    numeratorSum = int(sumDigits(num))
+    print("Time Elapsed:", time()-t0)
     print(numeratorSum)
 
 main()
--- Problem66.py
+++ Problem66.py
@@ -1,14 +1,21 @@
+#Problem: consider the Diophantine equation x^2-Dy^2=1. for any non-square value of D, we can find the minimum x value that has a y value that makes the
+#          equation true. So for D=3: 2^2-3*1^2 = 1 and for D=6: 5^2-6*2^2 = 1.
+#          Find the value of D<=1000 that has the largest corresponding minimum x value solution.
+
+#          Note: this Diophantine equation is Pell's equation, and shall be referred to as such
 import math
 from time import time
 
 
 def isSquare(x):
-    prec = .000001
-    rtx = math.sqrt(x)
-    if abs(rtx-int(rtx)) < prec:
+    #tells if a number is square
+    sqx = math.sqrt(x)
+    if int(sqx) == sqx:
         return True
     return False
+
 def getContinuedFractionOfSqrt(x):
+    #gets the continued fraction coefficients of sqrt(x)
     sqx = math.sqrt(x)
     ipart = int(sqx)
     
@@ -35,9 +42,15 @@ def getContinuedFractionOfSqrt(x):
     return [ipart] + cycle
 
 def testPellSolution(x,d,y):
+    #tests if a possible solution to Pell's equation is an actual solution
     return x*x-d*y*y == 1
 
 def findPellSolution(n):
+    #finds the minimum solution to Pell's equation where D = n. After doing some research (which I am having to do more and more), I found
+    #out that this is actually closely related to the convergents of continued fractions, which sort of makes sense, because the convergents
+    #are the best rational approximations of an irrational number, and that sort of fits in with Pell's equation.
+    #Anyway, the way to solve this is by creating convergents, and letting tx = p and ty = q where p is the numerator and q is the denominator.
+    #Then, we test if tx^2+D*ty^2=1. And the magical thing is that the first one that works is the smallest solution to Pell's equation
     alist = getContinuedFractionOfSqrt(n)
     convergents = []
     
@@ -72,12 +85,18 @@ def findPellSolution(n):
  
 
 def main():
+
+    #The strategy: Go through every non-square value J from 2 to 1000 and find the minimal x-value for the Pell equation with D=J
+    #Record the largest.
+    #Executes in .065 seconds
+    t0 = time()
     largest = [0,0]
     for i in range(2,1001):
         if not(isSquare(i)):
             minx = findPellSolution(i)[0]
             if minx > largest[1]:
                 largest = [i,minx]
+    print("Time Elapsed:", time()-t0)
     print(largest[0])
 main()
 

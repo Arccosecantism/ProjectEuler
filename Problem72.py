3dee5b9c4571f8699ce9535a0f08c485df4095a4
Riley Waugh <RWaugh9221@student.pps.net>
2017-03-08 09:31:47 -0800

    fixed problem 72 a bit, stil need to brush up commenting

--- Problem72.py
+++ Problem72.py
@@ -1,44 +1,48 @@
-
+#Problem: How many unique rational numbers p/q are there such that p,q are integers; 0<(p/q)<1; p,q < 1000000 (one million)? (We have to
+#            discount reductions: that is, 9/15 is not distinct from 3/5)
 import math
 from time import time
-def primeFactorSieve(x):
-    #Produces a list of numbers under x and the unique primes in each of those number's prime factorization
+def totientSieve(x):
+    #Produces a list of totients(n) for all n < x: uses a modified prime factor sieve
     ar = []
     for i in range(-1,x):
-        ar.append([])
+        ar.append(i+1)
     
     ub = int(x/2)+1
     for i in range(2,ub):
-        if ar[i] == []:
+        if ar[i] == i:
             for k in range(i,x,i):
-                ar[k].append(i)
-    for i in range(2,x+1):
-        if ar[i] == []:
-            ar[i] = [i]
+                ar[k]=int(ar[k]*(1-1.0/i))
+    for i in range(ub-1,x):
+        if ar[i] == i:
+            ar[i] -= 1
     return ar
 
 def totient(x, pfs):
-    primes = pfs[x]
-    totala = 1
-    totalb = 1
-    for i in primes:
-        
-        totala *= (i-1)
-        totalb *= i
+    #gives the number of natural numbers less than x that are relatively prime to x
+    totala = pfs[x][0]
+    totalb = pfs[x][1]
     return totala*1.0*x/totalb
 
 def main():
+    #The strategy: This is pretty obvious, especially since we covered totients earlier. A rational will be unique if it is irreducible. 
+    #A rational is irreducible if the numerator and denominator are relatively prime. So the number of rationals between 0 and 1 with a given denominator d
+    #is equal to the number of integers under d that are relatively prime to d, which is exactly the definithin of totient(d)=phi(d)
 
+    #So, we simply go from j=2 to 1000000, summing up totient(j)
+    #Executes in 7.131 seconds (I looked online, haven't seen much better times)
+    t0 = time()
     cap = 1000000
-    sieve = primeFactorSieve(cap+10)
+    sieve = totientSieve(cap+10)
     
-    #print(sieve)
+    print("here")
     #print(plist, sieve)
-    ssum = 0
+    ssum = sum(sieve[1:cap+1])-1
     
-    for i in range(2,cap+1):
+    #for i in range(2,cap+1):
         #print(totient(i,sieve))
-        ssum+=int(totient(i,sieve))
+    #    ssum+=sieve[i]
+    print("Time Elapsed:", time()-t0)
     print(ssum)
 
 main()

bfecf0b4fed8a42b2b93065d061f6c5a31303340
Riley Waugh <RWaugh9221@student.pps.net>
2017-03-14 09:35:53 -0700

    problem 79 how to do it?

--- Problem78.py
+++ Problem78.py
@@ -1,13 +1,16 @@
+#Problem: (equivalent to) what is the smallest n such that the number of integer partitions of n is divisible by 1,000,000?
 from time import time
 
+
 def getPentagonalNumbers(amt):
+    #gets amt generalized pentagonal numbers
     stop = int(amt/2)+1
     if amt%2:
         stop = -stop
     num = 1
     pentagList = []
     while num != stop:
-        pentagList.append((3*num*num-num)/2)
+        pentagList.append(int((3*num*num-num)/2))
         
         num = -num
         if num > 0:
@@ -15,35 +18,32 @@ def getPentagonalNumbers(amt):
         #print(stop,num)    
     return pentagList
 
-def numberOfPartitions(num, partList, pentList):
-    ssum = 0
-    ctr = 0
-    
-    while pentList[ctr] <= num:
-        val = partList[int(num-pentList[ctr])]
-        ms = ctr%4
-        if ms == 2 or ms ==3:
-            val = -val
-        ssum += val
-        ctr += 1
-    return ssum
-
 def main():
+    
+    #The strategy: simply generate all partitions of n (mod 1 million) untill one is divisible by 1 million
 
+    #Executes in 32.8 seconds
     t0 = time()
     pentagonList = getPentagonalNumbers(100000)
-    #divList = getListSumOfDivisors(200)
     print("done")
     partitionList = [1]
     ctr = 1
     val = 1
-    while val%1000000 != 0:
-        val = numberOfPartitions(ctr,partitionList,pentagonList)
-        #print(val)
+    sgnTab = [1,1,-1,-1]
+    while val != 0:
 
+        actr = 0
+        val = 0
+        
+        while pentagonList[actr] <= ctr:
+            val += sgnTab[actr%4]*partitionList[ctr-pentagonList[actr]]
+            actr += 1
+        
+        val = val%1000000
         partitionList.append(val)
-        ctr+=1
         print(ctr)
+        ctr+=1
+        
     
     print("Time Elapsed:", time()-t0)
     print(ctr-1)

4246745747eee89fc85f7f9a0150f750a1c3d793
xx_Archibald_xx <arcsine.assassin@gmail.com>
2017-03-16 10:36:21 -0700

    fixed some precision issues with problem 80

--- Problem80.py
+++ Problem80.py
@@ -13,6 +13,14 @@ def getSquareRootList(num):
     for i in range(start, len(strn), 2):
         digList.append(int(strn[i:i+2]))
     return digList
+def getNextDigit(lt, bt):
+    goodNum = 0
+    for i in range(1,11):
+        #print((lt+i)*i)
+        if (lt+i)*i > bt:
+            goodNum = i-1
+            break
+    return goodNum
 def manualSquareRootInt(num):
     #sqlist = manualSquareRootInt(num)
     leftTerm = 0
@@ -26,15 +34,17 @@ def manualSquareRootInt(num):
         else:
             leftTerm = int("".join(map(str,digitList)))*20
             bottomTerm = leftover*100
-        print("leftTerm:", leftTerm)
-        print("bottomTerm", bottomTerm)
-        nextDigit = int((math.sqrt(4*bottomTerm + leftTerm*leftTerm)-leftTerm)/2)
-        print("nextDigit:", nextDigit)
+        #print("leftTerm:", leftTerm)
+        #print("bottomTerm", bottomTerm)
+        nextDigit = getNextDigit(leftTerm, bottomTerm)
         digitList.append(nextDigit)
-        print("digitList:", digitList)
+        #print("digitList:", digitList)
+        if nextDigit == 0 and leftTerm < bottomTerm:
+            print("What?", leftTerm+1, bottomTerm)
         leftover = bottomTerm - (leftTerm+nextDigit)*nextDigit
-        print("leftover:", leftover)
-    return digList
+        #print("leftover:", leftover)
+        #print("divider:-------")
+    return digitList
 def main():
     print(manualSquareRootInt(2))
 

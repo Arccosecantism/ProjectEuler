b16bba8baf7cc66c4e5018fe98520c8cee174062
Arccosecantism <riley.d.waugh@gmail.com>
2017-03-20 07:40:15 -0700

    problem 81 failed, muat fix.

--- Problem81.py
+++ Problem81.py
@@ -7,73 +7,80 @@ import math
 import os
 from time import time
 
-def getNumList():
+def getNumMatrix():
 
 	#gets the list of numbers from "TriangleProblem67.txt"
 	print(os.getcwd())
-	f = open("TextFiles\TriangleProblem67.txt", 'r')
+	f = open("TextFiles\\MatrixTwoWayProblem81.txt", 'r')
 	lines = f.readlines()
-	nlines = []
+	flines = []
 	for i in lines:
-		nlines.append(i.split())
+		if i[-1] == "\n":
+			flines.append(i[:-1])
+		else:
+			flines.append(i)
+	nlines = [[int(i) for i in k.split(",")] for k in flines]
+	#for i in nlines:
+	#	print(i)
+	return nlines
 	
-	numar = []
-	for i in nlines:
-		numar.append([])
-		for k in i:
-			numar[len(numar)-1].append([int(k), 0])
-	return numar
-	
-	
-def getLeftChild(i, k, tri):
-	#gets the coordinates of the left child (straight down)
-	if i+1 >= len(tri) or i < 0:
+def getDownNode(i, k, matrix):
+	#gets the coordinates of the lower node
+	if i+1 >= len(matrix) or i < 0:
 		return [-1,-1]
-	elif k >= len(tri[i]) or k < 0:
+	elif k >= len(matrix[i]) or k < 0:
 		return [-1,-1]
 	return [i+1, k] 
-	
-def getRightChild(i, k, tri):
-	#gets the coordinates of the right child (straight right)
-	if i+1 >= len(tri) or i < 0:
-		return [-1,-1]
-	elif k >= len(tri[i]) or k < 0:
-		return [-1,-1]
-	return [i+1, k+1] 
 
-def findGreatestSum(i, k, numTriangle):
+def getRightNode(i, k, matrix):
+	#gets the coordinates of the right child (straight right)
+	if i >= len(matrix) or i < 0:
+		return [-1,-1]
+	elif k+1 >= len(matrix[i]) or k < 0:
+		return [-1,-1]
+	return [i, k+1] 
+
+def findGreatestSum(i, k, numMatrix, sumMatrix):
 	#recursively finds the greatest sum: it looks at the sums in [i][k]'s two children 
 	#and picks the bigger one and adds its value to it to figure out its own max sum
 	#If the children don't know their sum, recurse once on each child.
 	#So it's not total brute-force: it remembers values that it has already calculated
 	
-	if i >= 0 and i < len(numTriangle) and k >= 0 and k < len(numTriangle[i]):
+	if i >= 0 and i < len(numMatrix) and k >= 0 and k < len(numMatrix[i]):
 
-		if numTriangle[i][k][1] == 0:
-			if i == len(numTriangle) - 1:
-				numTriangle[i][k][1] = numTriangle[i][k][0]
-				#print("bottom: ", numTriangle[i][k][0])
+		if sumMatrix[i][k] == 0:
+			if i == len(numMatrix) - 1 and k == len(numMatrix[-1])-1:
+				sumMatrix[i][k] = numMatrix[i][k]
 				
 			else:
 				
-				lc = getLeftChild(i,k,numTriangle)
-				rc = getRightChild(i,k,numTriangle)
+				dn = getDownNode(i,k,numMatrix)
+				rn = getRightNode(i,k,numMatrix)
 				
-				findGreatestSum(lc[0],lc[1],numTriangle)
-				findGreatestSum(rc[0],rc[1],numTriangle)
+				if dn != [-1,-1]:
+					findGreatestSum(dn[0],dn[1],numMatrix,sumMatrix)
+				if rn != [-1,-1]:
+					findGreatestSum(rn[0],rn[1],numMatrix,sumMatrix)
 				
-				vlc = numTriangle[lc[0]][lc[1]]
-				vrc = numTriangle[rc[0]][rc[1]]
+				dval = -1
+				if dn != [-1,-1]:
+					dval = sumMatrix[dn[0]][dn[1]]
+				rval = -1
+				if rn != [-1,-1]:
+					rval = sumMatrix[rn[0]][rn[1]]
 				
 				#print(i,k,numTriangle[i][k][0],vlc,vrc)
 				maxVal = 0
 				
-				if  vlc[1] > vrc[1]:
-					maxVal = vlc[1]
-				else:
-					maxVal = vrc[1]
-					
-				numTriangle[i][k][1] = numTriangle[i][k][0]+maxVal
+				if not(dval == -1 and rval == -1):
+					if dval == -1:
+						maxVal = rval
+					elif rval == -1:
+						maxVal = dval
+					else:
+						maxVal = min(rval,dval)
+						
+					sumMatrix[i][k] = numMatrix[i][k]+maxVal
 	else:
 		print("ERROR")
 			
@@ -85,11 +92,20 @@ def main():
 	#worked for this problem, because I used some caching -- the program remembered previously calculated sums
 	#Executes in .09 seconds
 	t0 = time()
-	numTriangle = getNumList()
-	#print(numTriangle)
-	findGreatestSum(0,0,numTriangle)
+	numberMatrix = getNumMatrix()
+	pathMatrix = []
+	for i in range(0,len(numberMatrix)):
+		pathMatrix.append([])
+		for j in range(0,len(numberMatrix[i])):
+			pathMatrix[-1].append(0)
+	#pathMatrix[3][1] = 2
+	findGreatestSum(0,0,numberMatrix, pathMatrix)
 	print("Time Elapsed:", time()-t0)
-	print(numTriangle[0][0][1])
+	'''for i in numberMatrix:
+		print(i)
+	for i in pathMatrix:
+		print(i)'''
+	print(pathMatrix[0][0])
 			
 
 
---
+++ Problem82.py
@@ -0,0 +1,154 @@
+#Problem: 	Find the "path" (moving only down or down-right) in the trignle in "TriangleProblem67.txt" 
+#			that has the maximum sum compared to all other paths. What is this sum? (It might help to look at https://projecteuler.net/problem=18)
+#		
+#			This problem is almopst exactly the same as problem 18, except the triangle I have to search is much bigger, so I can't check every path -- I 
+#			need a better algorithm
+import math
+import os
+from time import time
+
+def getNumMatrix():
+
+    #gets the list of numbers from "TriangleProblem67.txt"
+    print(os.getcwd())
+    f = open("TextFiles\\MatrixThreeWayProblem82.txt", 'r')
+    lines = f.readlines()
+    flines = []
+    for i in lines:
+        if i[-1] == "\n":
+            flines.append(i[:-1])
+        else:
+            flines.append(i)
+    nlines = [[int(i) for i in k.split(",")] for k in flines]
+    #for i in nlines:
+    #	print(i)
+    return nlines
+	
+def minList(numlist):
+    minVal = numlist[0]
+    for i in numlist:
+        if i < minVal:
+            minVal = i
+    return minVal
+
+def getDownNode(i, k, matrix, blockMatrix):
+    #gets the coordinates of the lower node
+    if i+1 >= len(matrix) or i < 0:
+        return [-1,-1]
+    elif k >= len(matrix[i]) or k < 0:
+        return [-1,-1]
+    elif blockMatrix[i+1][k] == True:
+        return [-1,-1]
+    return [i+1, k] 
+
+def getUpNode(i,k,matrix, blockMatrix):
+    if i-1 >= len(matrix) or i-1 < 0:
+        return [-1,-1]
+    elif k >= len(matrix[i]) or k < 0:
+        return [-1,-1]
+    elif blockMatrix[i-1][k] == True:
+        return [-1,-1]
+    return [i-1,k]
+
+def getRightNode(i, k, matrix, blockMatrix):
+    #gets the coordinates of the right child (straight right)
+    if i >= len(matrix) or i < 0:
+        return [-1,-1]
+    elif k+1 >= len(matrix[i]) or k < 0:
+        return [-1,-1]
+    elif blockMatrix[i][k+1] == True:
+        return [-1,-1]
+    return [i, k+1] 
+
+def findLeastSum(i, k, numMatrix, sumMatrix, blockMatrix):
+    #recursively finds the greatest sum: it looks at the sums in [i][k]'s two children 
+    #and picks the bigger one and adds its value to it to figure out its own max sum
+    #If the children don't know their sum, recurse once on each child.
+    #So it's not total brute-force: it remembers values that it has already calculated
+    if i >= 0 and i < len(numMatrix) and k >= 0 and k < len(numMatrix[i]):
+        #print(i,k)
+        blockMatrix[i][k] = True
+
+        if sumMatrix[i][k] == 0:
+
+            if (k == len(numMatrix[-1])-1):
+                sumMatrix[i][k] = numMatrix[i][k]
+
+            else:
+
+                dirNodes = [[-1,-1],[-1,-1],[-1,-1]]
+                dlen = len(dirNodes)
+
+                dirNodes[0] = getDownNode(i,k,numMatrix,blockMatrix)
+                dirNodes[1] = getRightNode(i,k,numMatrix,blockMatrix)
+                dirNodes[2] = getUpNode(i,k,numMatrix,blockMatrix)
+
+                for j in dirNodes:
+                    if j != [-1,-1]:
+                        findLeastSum(j[0],j[1],numMatrix,sumMatrix,blockMatrix)
+
+
+                for j in dirNodes:
+                    blockMatrix[j[0]][j[1]] = False
+
+                dirVals = [-1,-1,-1]
+                for j in range(0,dlen):
+                    if dirNodes[j] != [-1,-1]:
+                        dirVals[j] = sumMatrix[dirNodes[j][0]][dirNodes[j][1]]
+                        if dirVals[j] == -1:
+                            sumMatrix[dirNodes[j][0]][dirNodes[j][1]] = 0
+
+                blocked = True
+                mtl = []
+                for j in dirVals:
+                    if j != -1:
+                        blocked = False
+                        mtl.append(j)
+                if blocked:
+                    sumMatrix[i][k] = -1
+                else:
+                    minVal = minList(mtl)
+                    sumMatrix[i][k] = numMatrix[i][k]+minVal
+
+    else:
+        print("ERROR:", i,k, len(numMatrix), len(numMatrix[i]))
+        #print((i >= 0 and i < len(numMatrix) and k >= 0 and k < len(numMatrix[i])))
+
+
+def main():
+	#see findGreatestSum for algorithm -- main simply calls it and prints the answer. THe way I solved the problem the first time
+	#worked for this problem, because I used some caching -- the program remembered previously calculated sums
+	#Executes in .09 seconds
+    t0 = time()
+    numberMatrix = getNumMatrix()
+    pathMatrix = []
+    backtrackMatrix = []
+
+    for i in range(0,len(numberMatrix)):
+        backtrackMatrix.append([])
+        pathMatrix.append([])
+        for j in range(0,len(numberMatrix[i])):
+            backtrackMatrix[-1].append(False)
+            pathMatrix[-1].append(0)
+	#pathMatrix[3][1] = 2
+
+    smallestSum = 0
+    findLeastSum(0,0,numberMatrix, pathMatrix,backtrackMatrix)
+    smallestSum = pathMatrix[0][0]
+    for i in range(1,len(numberMatrix)):
+        findLeastSum(i,0,numberMatrix, pathMatrix,backtrackMatrix)
+        nft = pathMatrix[i][0]
+        if nft < smallestSum:
+            smallestSum = nft
+    print("Time Elapsed:", time()-t0)
+    '''for i in numberMatrix:
+        print(i)
+    for i in pathMatrix:
+        print(i)
+    for i in backtrackMatrix:
+        print(i)'''
+    print(smallestSum)
+	
+main()
+
+

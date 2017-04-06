fea19b46b627e26cda0ea1bb1be4f7e3fbf016db
Riley Waugh <RWaugh9221@student.pps.net>
2017-04-06 09:47:28 -0700

    Finished 83, thank god

--- Problem83.py
+++ Problem83.py
@@ -11,7 +11,7 @@ def getNumMatrix():
 
     #gets the list of numbers from "TriangleProblem67.txt"
     print(os.getcwd())
-    f = open("TextFiles\\MatrixThreeWayProblem82Test2.txt", 'r')
+    f = open("TextFiles\\MatrixFourWayProblem83.txt", 'r')
     lines = f.readlines()
     flines = []
     for i in lines:
@@ -57,7 +57,22 @@ def getRightNode(i, k, matrix):
 def getLeftNode(i, k, matrix):
     return testIndex([i,k-1],matrix)
 
-def fixNodeNeighbors(i,k,markers):
+def fixNodeNeighbors(i,k,markers,nm,csm):
+    dnodes = []
+    dnodes.append(getRightNode(i,k,nm))
+    dnodes.append(getDownNode(i,k,nm))
+    dnodes.append(getLeftNode(i,k,nm))
+    dnodes.append(getUpNode(i,k,nm))
+    for node in dnodes:
+        if node != [-1,-1]:
+            if csm[node[0]][node[1]] > csm[i][k] + nm[node[0]][node[1]]:
+                csm[node[0]][node[1]] = csm[i][k] + nm[node[0]][node[1]]
+                markers.add((node[0],node[1]))
+    #markers.discard((i,k))
+
+
+
+
     
 def initializeCumulativeSumMatrix(nm, csm):
     csm[0][0] = nm[0][0]
@@ -75,11 +90,19 @@ def main():
     for i in range(0,len(numberMatrix)):
         cSumMatrix.append([0]*len(numberMatrix[i]))
     initializeCumulativeSumMatrix(numberMatrix,cSumMatrix)
-    markers = set([])
-    for i in numberMatrix:
-        print(i)
-    print('-----')
-    for i in cSumMatrix:
-        print(i)
+    fixMarkers = set([])
+
+    for i in range(0,len(cSumMatrix)):
+        for k in range(0,len(cSumMatrix[i])):
+            fixNodeNeighbors(i,k,fixMarkers,numberMatrix,cSumMatrix)
+ 
+    while len(fixMarkers) != 0:
+        ti = fixMarkers.pop()
+        fixNodeNeighbors(ti[0],ti[1],fixMarkers,numberMatrix,cSumMatrix)
+    minPathSum = cSumMatrix[-1][-1]
+    print("Elapsed Time:", time()-t0)
+    print(minPathSum)
+    
+
 
 main()
